37305ee780acaf7a7985657201f2e7dd1a95dd85
Srajan Shukla <154578887+srajanshukla@users.noreply.github.com>
2024-02-12 11:31:35 +0530

    ExcelDownload.java

--- src/main/java/com/society_Gate_Visitor_Registration/ExcelDownload/ExcelDownload.java
+++ src/main/java/com/society_Gate_Visitor_Registration/ExcelDownload/ExcelDownload.java
@@ -13,7 +13,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/visitors")
+@RequestMapping("/api/visitors")
 public class ExcelDownload {
 
     @Autowired
